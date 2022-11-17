ee88f2440784f69b9be820ecdbab3ee88946e37c
11Vasek1 <dmitry.navalov@mail.ru>
2022-11-17 05:12:29 +0300

    bugfix part 1

--- src/js/sections/partners/big-logo.js
+++ src/js/sections/partners/big-logo.js
@@ -1,44 +1,49 @@
 const container = document.querySelector('.big-logo__content-logo');
 
-import { data } from "./big-logo-data";
-import { popup } from "./popupLogo";
-import { createButton, createImg, createSubtitle, createDivBox} from "./utils";
-
+import { data } from './big-logo-data';
+import { popup } from './popupLogo';
+import { createButton, createImg, createSubtitle, createDivBox } from './utils';
 
 export function makeBigLogo() {
-  data.forEach(obj => {
-    const box = createBox(obj);
-    container.append(box);
-  });
-};
+	data.forEach((obj) => {
+		const box = createBox(obj);
+		container.append(box);
+	});
+}
 
 function createBox(obj) {
-  const button = createButton('big-logo__btn');
-  const imgBox = createDivBox('big-logo__img-box');
-  const imgWhite = createImg(obj.imgWhite, 'big-logo__img-white');
-  const imgBlack = createImg(obj.imgBlack, 'big-logo__img-black');
-  const imgColor = createImg(obj.imgColor, 'big-logo__img-color');
-  const imgColorWhite = createImg(obj.imgColorWhite, 'big-logo__img-color-white');
-  const textBox = createDivBox('big-logo__text-box');
-  const marker = createDivBox('big-logo__marker');
-  const subtitle = createSubtitle(obj.subtitle, ['text-s', 'big-logo__logo-text']);
+	const button = createButton('big-logo__btn');
+	const imgBox = createDivBox('big-logo__img-box');
+	const imgWhite = createImg(obj.imgWhite, 'big-logo__img-white');
+	const imgBlack = createImg(obj.imgBlack, 'big-logo__img-black');
+	const imgColor = createImg(obj.imgColor, 'big-logo__img-color');
+	const imgColorWhite = createImg(
+		obj.imgColorWhite,
+		'big-logo__img-color-white'
+	);
+	const textBox = createDivBox('big-logo__text-box');
+	const marker = createDivBox('big-logo__marker');
+	const subtitle = createSubtitle(obj.subtitle, [
+		'text-s',
+		'big-logo__logo-text',
+	]);
 
-  imgBox.append(imgWhite);
-  imgBox.append(imgBlack);
-  imgBox.append(imgColor);
-  imgBox.append(imgColorWhite);
+	imgBox.append(imgWhite);
+	imgBox.append(imgBlack);
+	imgBox.append(imgColor);
+	imgBox.append(imgColorWhite);
 
-  button.append(imgBox);
+	button.append(imgBox);
 
-  if (obj.subtitle != '') {
-    textBox.append(marker);
-    textBox.append(subtitle);
-    button.append(textBox);
-  }
+	if (obj.subtitle != '') {
+		textBox.append(marker);
+		textBox.append(subtitle);
+		button.append(textBox);
+	}
 
-  button.addEventListener('click', (event) => {
-    popup.open(obj);
-  });
+	button.addEventListener('click', (event) => {
+		popup.open(obj);
+	});
 
-  return button;
-};
+	return button;
+}
--- src/js/sections/partners/utils.js
+++ src/js/sections/partners/utils.js
@@ -1,27 +1,27 @@
-export function createButton(className) {  
-  const btn = document.createElement('button');
-  btn.classList.add(className);
-  return btn;
-};
+export function createButton(className) {
+	const btn = document.createElement('button');
+	btn.classList.add(className);
+	return btn;
+}
 
-export function createImg(src, className) {  
-const img = document.createElement('img');
-img.src = src;
-img.classList.add(className);
-return img;
-};
+export function createImg(src, className) {
+	const img = document.createElement('img');
+	img.src = src;
+	img.classList.add(className);
+	return img;
+}
 
-export function createSubtitle(textContent, classNames) {  
-const subtitle = document.createElement('p');
-subtitle.textContent = textContent;
-classNames.forEach((className) => {
-  subtitle.classList.add(className);
-});
-return subtitle;
-};
+export function createSubtitle(textContent, classNames) {
+	const subtitle = document.createElement('p');
+	subtitle.textContent = textContent;
+	classNames.forEach((className) => {
+		subtitle.classList.add(className);
+	});
+	return subtitle;
+}
 
-export function createDivBox(className) {  
-const divBox = document.createElement('div');
-divBox.classList.add(className);
-return divBox;
-};
+export function createDivBox(className) {
+	const divBox = document.createElement('div');
+	divBox.classList.add(className);
+	return divBox;
+}
